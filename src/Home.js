7171c62170840b6ba91fc5347cdde95fbf4c5127
shittu mohammed <moh.shittu@yahoo.com>
2021-11-04 20:05:50 +0100

    loading bug fixed

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Form from './SearchForm';
 import Movies from './Movies';
+import { useGlobalContext } from './context';
 const Home = () => {
+  const { loading } = useGlobalContext();
   return (
     <main>
       <Form />
 
       <Movies />
-      <footer>
-        <h4>App Developed by Modevsolutions || Api : OMDB</h4>
-      </footer>
+      {!loading && (
+        <footer>
+          <h4>App Developed by Modevsolutions || Api : OMDB</h4>
+        </footer>
+      )}
     </main>
   );
 };
